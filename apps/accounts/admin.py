d0b4e40e94312fb7ac607e5f0b363e98209274bb
Anirudhrarao <raorudhra16@gmail.com>
2026-06-30 22:01:25 +0530

    feat: implement authentication and user management

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,7 +1,64 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .models import User
+
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    """
+    Admin configuration for custom user model.
+    """
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "role",
+        "phone_number",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+    )
+
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "QuickIndia CRM",
+            {
+                "fields": (
+                    "role",
+                    "phone_number",
+                    "profile_image",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "QuickIndia CRM",
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "role",
+                    "phone_number",
+                ),
+            },
+        ),
+    )
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,32 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
     """
     Custom user model.
     """
-    pass
+    
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Administrator"
+        SALES_EXECUTIVE = "SALES_EXECUTIVE", "Sales Executive"
+        SALES_MANAGER = "SALES_MANAGER", "Sales Manager"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.SALES_EXECUTIVE,
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True,
+    )
+
+    profile_image = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.get_full_name() or self.username
